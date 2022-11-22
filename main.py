13dc11652518434ddb2d1cb6076ea63dcb923955
nifist99 <nikofigit@gmail.com>
2022-11-22 19:52:49 +0700

    done main'

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-from run import main
+import os
+import pathlib
+from os.path import exists
+from bs4 import BeautifulSoup
 from run_api import main_api
+from run import main
+import glob
+import os
+import requests
+import json
+from module.scrapping import Scrap
+from module.setting import Config
+import chromedriver_autoinstaller
+from module.excel import Excel
+from module.scripping_api import Scrap_API
+from linkedin_scraper import Person,Company
+import pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from api.api_web import Api
+import requests
+import json
+import shutil
+from module.excel import Excel
+from module.helper import Helper
+from linkedin_api import Linkedin
+import time
+from tkinter import *
+import re
+import pandas as pd
 import os
 
-def bot():
+import chromedriver_autoinstaller
+
+from module.setting import Config
+
+path = pathlib.Path(__file__).parent.resolve()
+
+
+
+def main():
     print('#*********** WELCOME TO BOT LINKED IND ***************#')
     print("ini ada 2 metode by API DAN WEB")
     print()
@@ -20,6 +59,7 @@ def bot():
     elif code == 2:
         os.system("cls")
         main()
+
     
     else:
         os.system("cls")
@@ -27,9 +67,7 @@ def bot():
         exit()
 
 if __name__ == "__main__":
-
-
-    bot()
+    main()
 
 
 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='linked_nfs',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='nikofigit@gmail.com',
+license='MIT',
+packages=['linked_nfs'],
+zip_safe=False)
